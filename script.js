ad8bbb78cb92e45c58e826f9dc989e9124840f99
Prajwal2246 <janbandhuprajwal28@gmail.com>
2025-08-17 21:25:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+// your JS code here. If required.
+
+document.getElementById("btn").addEventListener("click", function () {
+	let ageInput = document.getElementById("age").value.trim();
+	let nameInput = document.getElementById("name").value.trim();
+
+	if (!nameInput || !ageInput) {
+		alert("Please enter valid details.");
+		return;
+	}
+
+	const age = parseInt(ageInput);
+	const name = nameInput;
+
+	const votingPromise = () => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				if (age > 18) {
+					resolve(`Welcome, ${name}. You can vote.`);
+				} else {
+					reject(`Oh sorry ${name}. You aren't old enough.`);
+				}
+			}, 4000);
+		});
+	};
+
+	votingPromise()
+		.then((message) => {
+			alert(message);
+		})
+		.catch((error) => {
+			alert(error);
+		});
+});
